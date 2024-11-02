bb94818beb5d08a5fc0f7ee4123de9b35ace48fb
Ibrahim Khan <randomcricketpakistan@gmail.com>
2024-11-01 22:00:35 -0700

    ignore txt
    TA: ".txt files are not distinctly webpages. Text files are in the same cateogry as PDFs in this regard."

--- scraper.py
+++ scraper.py
@@ -73,7 +73,7 @@ def is_valid(url):
             + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
             + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
             + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
-            + r"|epub|dll|cnf|tgz|sha1"
+            + r"|epub|dll|cnf|tgz|sha1|txt"
             + r"|thmx|mso|arff|rtf|jar|csv|ppsx"
             + r"|rm|smil|wmv|swf|wma|zip|rar|gz|apk|war|img|sql)$", parsed.path.lower()):
             return False
