847aa4c978f39f7d4270fd231bee9e8b7039f0d6
Ibrahim Khan <randomcricketpakistan@gmail.com>
2024-11-04 20:21:53 -0800

    cleaned some imports

--- crawler/frontier.py
+++ crawler/frontier.py
@@ -1,9 +1,5 @@
 import os
 import shelve
-
-from threading import Thread, RLock
-from queue import Queue, Empty
-
 from utils import get_logger, get_urlhash, normalize
 from scraper import is_valid
 from urllib.parse import urldefrag
