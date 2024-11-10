3ee26bee6cb5a280658e1e6e95409993bae89a9b
Ibrahim Khan <randomcricketpakistan@gmail.com>
2024-11-09 23:35:33 -0800

    further improved

--- scraper.py
+++ scraper.py
@@ -66,7 +66,7 @@ def is_valid(url):
             return False
         
         # get rid of traps i fell into
-        if any(param in parsed.query for param in ["do=media", "image=", "ical=1", "outlook-ical=1", "tribe-bar-date", "redirect", "share=", "filter", "action=download", "action=login", "idx=", "version="]):
+        if any(param in parsed.query for param in ["do=media", "image=", "ical=1", "outlook-ical=1", "tribe-bar-date", "redirect", "share=", "filter", "action=download", "action=login", "idx=", "version=", "precision="]):
             return False
         
         if any(param in parsed.path for param in ["/-/commit", "/-/blob", "/-/blame", "/-/tree", "/-/merge_requests", "~eppstein/pix/"]):
